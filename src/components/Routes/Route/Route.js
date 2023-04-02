87eb9311a0d5e4a3471d51f6c8b45490ae1fd5b8
RedEye75 <yusufbinemamstk@gmail.com>
2023-04-02 20:28:19 +0600

    background wise teacher

--- src/components/Routes/Route/Route.js
+++ src/components/Routes/Route/Route.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layouts/Main/Main";
 import Home from "../../Pages/Home/Home";
 import Login from "../../Pages/Authentication/Login/Login";
@@ -9,79 +9,74 @@ import Dashboard from "../../Layouts/Dashboard/Dashboard";
 import WelcomeDashboard from "../../Pages/WelcomeDashboard/WelcomeDashboard";
 import TeacherSignup from "../../Pages/Authentication/TeacherSignup/TeacherSignup";
 import StudentSignup from "../../Pages/Authentication/StudentSignup/StudentSignup";
+import SubjectWiseTeacher from "../../Pages/SubjectWiseTeacher/SubjectWiseTeachers";
 
 const router = createBrowserRouter([
-          {
-            path: "/", element: <Main />,
-            children: [
-              {
-                path: "/", element: <Home />,
-              },
-              {
-                path: "/login", element: <Login />,
-              },
-              {
-                path: "/signup-teacher", element: <TeacherSignup />,
-              },
-              {
-                path: "/signup-student", element: <StudentSignup />,
-              },
-              {
-                path: "/about-us", element: <About />,
-              },
-              {
-                path: "/blog", element: <Blog />,
-              },
-              // {
-              //   path: "/contact", element: <Contact />,
-              // },
-            ],
-          },
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup-teacher",
+        element: <TeacherSignup />,
+      },
+      {
+        path: "/signup-student",
+        element: <StudentSignup />,
+      },
+      {
+        path: "/about-us",
+        element: <About />,
+      },
+      {
+        path: "/blog",
+        element: <Blog />,
+      },
+      // {
+      //   path: "/contact", element: <Contact />,
+      // },
+      {
+        path: "/teachers",
+        element: <SubjectWiseTeacher></SubjectWiseTeacher>,
+      },
+    ],
+  },
 
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+    children: [
+      {
+        path: "/dashboard",
+        element: <WelcomeDashboard />,
+      },
+      //   {
+      //     path: "/dashboard/profile", element: <MyProfile />,
+      //   },
+      //   {
+      //     path: "/dashboard/add-teacher", element: <AddTeacher />,
+      //   },
+      //   {
+      //     path: "/dashboard/all-teachers", element: <AllTeachers />,
+      //   },
+      //   {
+      //     path: "/dashboard/all-students", element: <AllStudents />,
+      //   },
+    ],
+  },
 
-
-          {
-            path: "/dashboard", element: <Dashboard />,
-            children: [
-              {
-                path: "/dashboard", element: <WelcomeDashboard />,
-              },
-            //   {
-            //     path: "/dashboard/profile", element: <MyProfile />,
-            //   },
-            //   {
-            //     path: "/dashboard/add-teacher", element: <AddTeacher />,
-            //   },
-            //   {
-            //     path: "/dashboard/all-teachers", element: <AllTeachers />,
-            //   },
-            //   {
-            //     path: "/dashboard/all-students", element: <AllStudents />,
-            //   },
-            ]
-          },
-
-
-
-
-
-
-
-
-          {
-            path: "*",
-            element: <div>Error 404 page</div>,
-          }
-
-
-
-
-
-
-
-
-
-
+  {
+    path: "*",
+    element: <div>Error 404 page</div>,
+  },
 ]);
 
 export default router;
