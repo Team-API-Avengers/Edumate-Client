c336f7e8d5d0d26131874fadc4014d86c5186906
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2023-04-02 20:52:42 +0600

    n

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import SubjectWiseTeacher from '../SubjectWiseTeacher/SubjectWiseTeacher';
 
 const Home = () => {
     return (
         <div>
             <Banner />
+            <SubjectWiseTeacher />
         </div>
     );
 };
--- src/components/Routes/Route/Route.js
+++ src/components/Routes/Route/Route.js
@@ -28,6 +28,7 @@ const router = createBrowserRouter([
               {
                 path: "/blog", element: <Blog />,
               },
+             
             ],
           },
 
