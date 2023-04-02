4fcdf2b7f777ecba4904089ea1ce76fcfe350d5c
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2023-04-02 17:08:08 +0600

    header section added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <button className="btn">Button</button>
-<button className="btn btn-primary">Button</button>
-<button className="btn btn-secondary">Button</button>
-<button className="btn btn-accent">Button</button>
-<button className="btn btn-ghost">Button</button>
-<button className="btn btn-link">Button</button>
+     <Header />
     </div>
   );
 }
