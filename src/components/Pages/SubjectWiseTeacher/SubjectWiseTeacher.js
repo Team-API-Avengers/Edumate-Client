b814bc3a42fbd69e668b7828f89af9bfa6f297c4
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2023-04-02 19:32:54 +0600

    n

---
+++ src/components/Pages/SubjectWiseTeacher/SubjectWiseTeacher.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SubjectWiseTeacher = () => {
+    return (
+        <div>
+            <h1>SubjectWiseTeacher</h1>
+        </div>
+    );
+};
+
+export default SubjectWiseTeacher;
