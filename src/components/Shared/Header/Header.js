4e907f1d107e40f36c85fdf66fe3450ff4bc8f5c
RedEye75 <yusufbinemamstk@gmail.com>
2023-04-04 02:59:46 +0600

    modified best teacher section

--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -13,6 +13,7 @@ import { BsChevronUp } from "react-icons/bs";
 import { color } from "style-value-types";
 import { AuthContext } from "../../Context/AuthProvider";
 import { toast } from "react-toastify";
+import logo from "../../Assets/edumateLogo.png";
 
 const Header = () => {
   const { user, logOut } = useContext(AuthContext);
@@ -90,58 +91,12 @@ const Header = () => {
 
   return (
     <div>
-      {/* <Navbar className="sticky bg-slate-200 text-black inset-0 z-10 h-max max-w-full rounded-none py-2 px-4 lg:px-8 lg:py-4">
-        <div className="flex items-center justify-between text-blue-gray-900">
-          <Typography
-            as="a"
-            href="#"
-            className="mr-4 cursor-pointer py-1.5 font-medium"
-          >
-            Edumate
-          </Typography>
-          <div className="flex items-center gap-4">
-            <div className="mr-4 hidden lg:block">{navList}</div>
-
-            <div className="dropdown dropdown-end">
-              <label tabIndex={0} className="cursor-pointer">
-                <div className="avatar online">
-                  <div className="w-12 rounded-full  ring ring-primary ring-offset-base-100 ring-offset-2">
-                    <img src="https://png.pngitem.com/pimgs/s/44-446384_north-carolina-tar-heels-duke-blue-devils.png" />
-                  </div>
-                </div>
-              </label>
-              <ul
-                tabIndex={0}
-                className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
-              >
-                <li>
-                  <a className="justify-between">
-                    Profile
-                    <span className="badge">New</span>
-                  </a>
-                </li>
-                <li>
-                  <Link to="/">Settings</Link>
-                </li>
-                <li>
-                  <a>Logout</a>
-                </li>
-
-                <li>
-                  <Link to="/login">Login</Link>
-                </li>
-                <li>
-                  <Link to="/signup">Signup</Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </Navbar> */}
       <Navbar className="mx-auto shadow-lg max-w-screen-2xl bg-black py-6 px-4 rounded-none lg:px-8 lg:py-6">
         <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
           <Link to={"/"}>
-            <span className="text-4xl font-bold">Edumate</span>
+            <span className="text-4xl font-bold">
+              <img className="w-16" src={logo} alt="logo" />
+            </span>
           </Link>
 
           <div className="hidden lg:block">{navList}</div>
