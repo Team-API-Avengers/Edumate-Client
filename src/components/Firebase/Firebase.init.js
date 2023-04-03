57f496932cd8598c55c6ab7d26bc182efa8bcbfa
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2023-04-03 19:32:25 +0600

    firebase setup

---
+++ src/components/Firebase/Firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDljQabwXd8acg4_022NYQSxdaglHGCogk",
+  authDomain: "edumate-53dfb.firebaseapp.com",
+  projectId: "edumate-53dfb",
+  storageBucket: "edumate-53dfb.appspot.com",
+  messagingSenderId: "73505704580",
+  appId: "1:73505704580:web:0f45f8c929d2d30e8890d1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
--- src/components/Pages/Home/OurEnvironment/OurEnvironment.js
+++ src/components/Pages/Home/OurEnvironment/OurEnvironment.js
@@ -9,14 +9,14 @@ const OurEnvironment = () => {
 
 
                 <div className='relative w-1/2'>
-                <img src="https://www.tutorstorm.com/wp-content/uploads/2020/08/subject-tutoring-1.jpg" className="max-w-sm w-32 object-cover rotate-45 h-32 absolute top-[-120px] left-16  border-b-4 border-r-8 border-purple-600 rounded-lg shadow-2xl" />
+                <img src="https://img1.wsimg.com/isteam/ip/e371c7f8-92b2-4636-8484-5c44ea3c30f7/blob-f732d4d.png" className="max-w-sm w-32 ob rotate-45 h-32 absolute top-[-120px] left-16  border-b-4 border-r-8 border-purple-600 rounded-lg shadow-2xl" />
 
-                <img src="https://www.tutorstorm.com/wp-content/uploads/2020/08/exam-and-sac-preparation-1.jpg" className="max-w-sm w-36 rotate-45 h-32 absolute top-0 right-48 object-cover border-b-4 border-r-8 border-green-600   rounded-lg shadow-2xl" />
+                <img src="https://llt.imgix.net/v1/1657590644-varsity-tutors-for-schools-teacher-assigned-one-pager-edited.png?ixlib=rb-3.3.1&fit=&auto=format%2Ccompress&w=960&crop=focalpoint&fp-x=0.6&fp-y=0.97&h=540&ar=1.7777777777777777" className="max-w-sm w-36 rotate-45 h-32 absolute top-0 right-48 border-b-4 border-r-8 border-green-600   rounded-lg shadow-2xl" />
 
 
-                <img src="http://hometutor.co.tz/wp-content/uploads/2020/02/HOME-TUTOR-LOGO-01.png" className="max-w-sm w-28 border-b-4 border-r-8 border-orange-600 rotate-45 h-28 object-cover absolute bottom-32 left-36 rounded-lg shadow-2xl" />
+                <img src="http://hometutor.co.tz/wp-content/uploads/2020/02/HOME-TUTOR-LOGO-01.png" className="max-w-sm w-28 border-b-4 border-r-8 border-orange-600 rotate-45 h-28 absolute bottom-32 left-36 rounded-lg shadow-2xl" />
 
-                <img src="https://www.tutorstorm.com/wp-content/uploads/2020/08/online-tutoring.jpg" className="max-w-sm w-52 rotate-45 h-52 border-b-4 border-r-8 border-blue-700 object-cover absolute bottom-[-10px] right-12 rounded-lg shadow-2xl" />
+                <img src="https://content.jdmagicbox.com/comp/lucknow/d2/0522px522.x522.171206135533.c5d2/catalogue/go-home-tutor-gomti-nagar-lucknow-tutorials-irvx9.jpg" className="max-w-sm w-52 rotate-45 h-52 border-b-4 border-r-8 border-blue-700 absolute bottom-[-10px] right-12 rounded-lg shadow-2xl" />
                 </div>
 
 
