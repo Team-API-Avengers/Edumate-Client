e8ab7f2234a2c8c7afc7978a562bf38901e93b0a
RedEye75 <yusufbinemamstk@gmail.com>
2023-04-05 22:00:58 +0600

    modified

--- src/components/Pages/Blog/Blog.js
+++ src/components/Pages/Blog/Blog.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Blog = () => {
-  const [blogs, setBlog] = useState({});
+  const [blogs, setBlog] = useState(null);
   useEffect(() => {
     fetch("blogdata.json")
       .then((res) => res.json())
@@ -11,40 +11,42 @@ const Blog = () => {
   return (
     <div>
       <div className="max-w-screen-xl mx-auto grid grid-cols-2 gap-5">
-        {blogs.map((singleBlog) => (
-          <article class="rounded-xl bg-white p-4 ring ring-indigo-50 sm:p-6 lg:p-8">
-            <div class="flex items-start sm:gap-8">
-              <div>
-                <strong class="rounded border text-start text-white border-blue-500 bg-blue-500 px-3 py-1.5 text-[10px] font-medium ">
-                  {singleBlog.blogCategory}
-                </strong>
+        {blogs?.map((singleBlog, i) => (
+          <Link to={`blogDetails/${i}`}>
+            <article class="rounded-xl bg-white p-4 ring ring-indigo-50 sm:p-6 lg:p-8">
+              <div class="flex items-start sm:gap-8">
+                <div>
+                  <strong class="rounded border text-start text-white border-blue-500 bg-blue-500 px-3 py-1.5 text-[10px] font-medium ">
+                    {singleBlog?.blogCategory}
+                  </strong>
 
-                <h3 class="mt-4 text-2xl  text-start font-bold ">
-                  <Link to={"/"} class="hover:underline">
+                  <h3 class="mt-4 text-2xl hover:underline text-start font-bold ">
                     {singleBlog?.title}
-                  </Link>
-                </h3>
+                  </h3>
 
-                <p class="mt-1 text-start text-sm text-gray-700">
-                  {singleBlog?.blogDetails.slice(0, 98)} .....
-                </p>
+                  <p class="mt-1 text-start text-sm text-gray-700">
+                    {singleBlog?.blogDetails.slice(0, 98)}.....
+                  </p>
 
-                <div class="mt-4 flex justify-between">
-                  <div>
-                    <p className="text-start font-bold">{singleBlog?.author}</p>
-                    <p className="text-start font-medium">
-                      {singleBlog?.authorProfession}
-                    </p>
-                  </div>
-                  <div class="flex items-center gap-1 text-gray-500">
-                    <p class="text-md text-black  font-medium">
-                      {singleBlog?.date}
-                    </p>
+                  <div class="mt-4 flex justify-between">
+                    <div>
+                      <p className="text-start font-bold">
+                        {singleBlog?.author}
+                      </p>
+                      <p className="text-start font-medium">
+                        {singleBlog?.authorProfession}
+                      </p>
+                    </div>
+                    <div class="flex items-center gap-1 text-gray-500">
+                      <p class="text-md text-black  font-medium">
+                        {singleBlog?.date}
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </article>
+            </article>
+          </Link>
         ))}
       </div>
     </div>
