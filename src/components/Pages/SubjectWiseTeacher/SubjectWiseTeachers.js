9d30aa11805431cc9792eb7bdd0244b4abb13e38
RedEye75 <yusufbinemamstk@gmail.com>
2023-04-07 23:08:08 +0600

    review section modified

--- src/components/Pages/SubjectWiseTeacher/SubjectWiseTeachers.js
+++ src/components/Pages/SubjectWiseTeacher/SubjectWiseTeachers.js
@@ -8,96 +8,96 @@ import all from "../../Assets/all.png";
 import { useEffect, useState } from "react";
 
 const SubjectWiseTeachers = () => {
-  const [backgrounds, setBackground] = useState([]);
-  useEffect(() => {
-    fetch(`https://edumate-second-server.vercel.app/api/v1/tutor`).then((res) =>
-      res.json().then((data) => setBackground(data?.data))
-    );
-  }, []);
+  // const [backgrounds, setBackground] = useState([]);
+  // useEffect(() => {
+  //   fetch(`https://edumate-second-server.vercel.app/api/v1/tutor`).then((res) =>
+  //     res.json().then((data) => setBackground(data?.data))
+  //   );
+  // }, []);
   return (
     <div>
       <h1 className="text-3xl font-bold">Department</h1>
       <p className="mt-2">Select which department you need a teacher for</p>
-      {backgrounds.map((category) => (
-        <div className=" max-w-screen-lg mt-10 mb-10 mx-auto">
-          <div class="container grid grid-cols-2 gap-10 px-5 py-1  mx-auto">
-            <Link>
-              <div class=" flex justify-between items-center rounded-lg border bg-white border-gray-200 p-4 h-36   hover:border-green-500 ">
-                <div className="flex">
-                  <img className="w-14 mt-3 h-12" src={science} alt="" />
-                  <div className="ml-5">
-                    <h2 class="mt-2 text-start  text-xl font-bold">Science</h2>
+      {/* {backgrounds.map((category) => ( */}
+      <div className=" max-w-screen-lg mt-10 mb-10 mx-auto">
+        <div class="container grid grid-cols-2 gap-10 px-5 py-1  mx-auto">
+          <Link to={"/Science"}>
+            <div class=" flex justify-between items-center rounded-lg border bg-white border-gray-200 p-4 h-36   hover:border-green-500 ">
+              <div className="flex">
+                <img className="w-14 mt-3 h-12" src={science} alt="" />
+                <div className="ml-5">
+                  <h2 class="mt-2 text-start  text-xl font-bold">Science</h2>
 
-                    <p class="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
-                      10 teachers available
-                    </p>
-                  </div>
-                </div>{" "}
-                <div className="mr-5">
-                  <IoIosArrowForward />
+                  <p class="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+                    10 teachers available
+                  </p>
                 </div>
+              </div>{" "}
+              <div className="mr-5">
+                <IoIosArrowForward />
               </div>
-            </Link>
-            <Link>
-              <div class=" flex justify-between items-center rounded-lg border  bg-white border-gray-200 p-4 h-36   hover:border-green-500 ">
-                <div className="flex">
-                  <img className="w-14 mt-3 h-12" src={commerce} alt="" />
-                  <div className="ml-5">
-                    <h2 class="mt-2 text-start  text-xl font-bold">Commerce</h2>
+            </div>
+          </Link>
+          <Link to={"/Commerce"}>
+            <div class=" flex justify-between items-center rounded-lg border  bg-white border-gray-200 p-4 h-36   hover:border-green-500 ">
+              <div className="flex">
+                <img className="w-14 mt-3 h-12" src={commerce} alt="" />
+                <div className="ml-5">
+                  <h2 class="mt-2 text-start  text-xl font-bold">Commerce</h2>
 
-                    <p class="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
-                      10 teachers available
-                    </p>
-                  </div>
-                </div>{" "}
-                <div className="mr-5">
-                  <IoIosArrowForward />
+                  <p class="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+                    10 teachers available
+                  </p>
                 </div>
+              </div>{" "}
+              <div className="mr-5">
+                <IoIosArrowForward />
               </div>
-            </Link>
-            <Link>
-              <div class=" flex justify-between items-center rounded-lg  border bg-white border-gray-200 p-4 h-36   hover:border-green-500 ">
-                <div className="flex">
-                  <img className="w-14 mt-3 h-12" src={humanity} alt="" />
-                  <div className="ml-5">
-                    <h2 class="mt-2 text-start  text-xl font-bold">Humanity</h2>
+            </div>
+          </Link>
+          <Link to={"/Humanity"}>
+            <div class=" flex justify-between items-center rounded-lg  border bg-white border-gray-200 p-4 h-36   hover:border-green-500 ">
+              <div className="flex">
+                <img className="w-14 mt-3 h-12" src={humanity} alt="" />
+                <div className="ml-5">
+                  <h2 class="mt-2 text-start  text-xl font-bold">Humanity</h2>
 
-                    <p class="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
-                      10 teachers available
-                    </p>
-                  </div>
-                </div>{" "}
-                <div className="mr-5">
-                  <IoIosArrowForward />
+                  <p class="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+                    10 teachers available
+                  </p>
                 </div>
+              </div>{" "}
+              <div className="mr-5">
+                <IoIosArrowForward />
               </div>
-            </Link>{" "}
-            <Link to={"/all-Teachers"}>
-              <div class=" flex justify-between items-center rounded-lg border bg-white border-gray-200 p-4 h-36   hover:border-green-500 ">
-                <div className="flex">
-                  <img className="w-14 mt-3 h-12" src={all} alt="" />
-                  <div className="ml-5">
-                    <h2 class="mt-2 text-start  text-xl font-bold">
-                      All Department
-                    </h2>
+            </div>
+          </Link>{" "}
+          <Link to={"/all-Teachers"}>
+            <div class=" flex justify-between items-center rounded-lg border bg-white border-gray-200 p-4 h-36   hover:border-green-500 ">
+              <div className="flex">
+                <img className="w-14 mt-3 h-12" src={all} alt="" />
+                <div className="ml-5">
+                  <h2 class="mt-2 text-start  text-xl font-bold">
+                    All Department
+                  </h2>
 
-                    <p class="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
-                      10 teachers available
-                    </p>
-                  </div>
-                </div>{" "}
-                <div className="mr-5">
-                  <IoIosArrowForward />
+                  <p class="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+                    10 teachers available
+                  </p>
                 </div>
+              </div>{" "}
+              <div className="mr-5">
+                <IoIosArrowForward />
               </div>
-            </Link>
-          </div>
+            </div>
+          </Link>
+        </div>
 
-          {/* {teachers.map((teacher, i) => (
+        {/* {teachers.map((teacher, i) => (
           <SubjectWiseTeacher key={i} teacher={teacher}></SubjectWiseTeacher>
         ))} */}
-        </div>
-      ))}
+      </div>
+      {/* ))} */}
     </div>
   );
 };
