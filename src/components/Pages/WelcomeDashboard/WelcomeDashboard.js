34e4142c09b009236bca7e2aaf036780b210ee5c
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2023-04-07 11:47:30 +0600

    welcome dashboard

--- src/components/Pages/WelcomeDashboard/WelcomeDashboard.js
+++ src/components/Pages/WelcomeDashboard/WelcomeDashboard.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import logo from '../../Assets/edumateLogo.png'
 
 const WelcomeDashboard = () => {
     return (
-        <div>
-            <h1>Welcome Dashboard</h1>
-        </div>
+       			<div
+				className='hero min-h-screen'
+				style={{
+					backgroundImage: `url("https://cdn.dribbble.com/users/1195128/screenshots/14971635/__________________1.png")`,
+				}}
+			>
+				<div className='hero-overlay bg-opacity-30'></div>
+				<div className='hero-content text-center text-neutral-content'>
+					<div className='max-w-md'>
+						<h1 className='mb-5 text-4xl font-bold text-white'>
+							Welcome to Deshboard
+						</h1>
+					</div>
+				</div>
+			</div>
     );
 };
 
