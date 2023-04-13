c4261b27cf3b60f803b23735cf323743cedca7e3
atik94 <atikurrahmanatik94@gmail.com>
2023-04-14 01:43:56 +0600

    Blog page animation completed

--- src/components/Pages/Blog/Blog.js
+++ src/components/Pages/Blog/Blog.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../Context/AuthProvider";
 import Loader from "../../Shared/Loader/Loader";
+import { motion } from "framer-motion";
 
 const Blog = () => {
   const { loading } = useContext(AuthContext);
@@ -19,14 +20,26 @@ const Blog = () => {
   }
 
   return (
-    <div className="m-5 min-h-screen">
-      <h1 className="text-5xl font-bold">
+    <div className="m-4 min-h-screen">
+      <motion.h1
+        className=" font-bold"
+        initial={{ scale: 0.5 }}
+        whileInView={{ scale: 2.1, translateY: 20 }}
+        transition={{ duration: 1.5 }}
+      >
         Popular <span className="border-b-8 border-blue-600">Articles</span>
-      </h1>
-      <div className="max-w-screen-xl mx-auto mt-10 grid grid-cols-1 lg:grid-cols-2 gap-5">
+      </motion.h1>
+      <motion.div
+        className="max-w-screen-xl mx-auto mt-36 grid grid-cols-1 lg:grid-cols-2 gap-5"
+        whileInView={{ translateY: -70 }}
+        transition={{ duration: 2 }}
+      >
         {blogs?.map((singleBlog, i) => (
           <Link to={`/blog/${singleBlog?._id}`}>
-            <article class="rounded-xl border h-72 border-green-100  bg-white p-4 ring ring-indigo-50 sm:p-6 lg:p-8">
+            <motion.article
+              class="rounded-xl border h-72 border-green-100  bg-white p-4 ring ring-indigo-50 sm:p-6 lg:p-8"
+              whileHover={{ background: "linear-gradient(270deg, #439370 0%, #3a3276 100%)", color: "white" }}
+            >
               <div class="flex items-start sm:gap-8">
                 <div>
                   <strong class="rounded border text-start text-white border-blue-500 bg-blue-500 px-3 py-1.5 text-[10px] font-medium ">
@@ -37,32 +50,28 @@ const Blog = () => {
                     {singleBlog?.title}
                   </h3>
 
-                  <p class="mt-1 text-start text-sm text-gray-700">
+                  <p class="mt-1 text-start text-sm text-gray-400 hover:text-base-100">
                     {singleBlog?.details?.slice(0, 98)}.....
                   </p>
 
                   <div class="mt-4 flex  justify-between">
                     <div>
-                      <p className="text-start font-bold">
-                        {singleBlog?.authorName}
-                      </p>
-                      <p className="text-start font-medium">
-                        {singleBlog?.authorRole}
-                      </p>
+                      <p className="text-start font-bold">{singleBlog?.authorName}</p>
+                      <p className="text-start font-medium">{singleBlog?.authorRole}</p>
                     </div>
                     <div class="flex items-center gap-1 text-gray-500">
                       <p class="text-md text-black  font-medium">
-                      <span className="mx-1">{singleBlog?.createdAt?.slice(11,16)}</span>
-                        <span className="mx-1">{singleBlog?.createdAt?.slice(0,10)}</span>
+                        <span className="mx-1">{singleBlog?.createdAt?.slice(11, 16)}</span>
+                        <span className="mx-1">{singleBlog?.createdAt?.slice(0, 10)}</span>
                       </p>
                     </div>
                   </div>
                 </div>
               </div>
-            </article>
+            </motion.article>
           </Link>
         ))}
-      </div>
+      </motion.div>
     </div>
   );
 };
