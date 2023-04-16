bf4d37fe589bef2ffe5c8cf3fec75a3484b015f9
atik94 <atikurrahmanatik94@gmail.com>
2023-04-17 00:03:58 +0600

    Subject wise teacher responsibe and about us page animate completed

--- src/components/Pages/Instructors/Instructors.js
+++ src/components/Pages/Instructors/Instructors.js
@@ -1,11 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { AiFillStar } from "react-icons/ai";
-import {
-  BsArrowRight,
-  BsArrowRightShort,
-  BsFillPersonFill,
-  BsHourglassSplit,
-} from "react-icons/bs";
+import { BsArrowRight, BsArrowRightShort, BsFillPersonFill, BsHourglassSplit } from "react-icons/bs";
 import { FaBookReader } from "react-icons/fa";
 import { HiLocationMarker } from "react-icons/hi";
 import { Link } from "react-router-dom";
@@ -28,7 +23,7 @@ const Instructors = () => {
   }
   return (
     <div className="m-5">
-      <div className="grid lg:grid-cols-4 sm:grid-cols-1 md:grid-cols-2 md:gap-6  gap-4 max-w-screen-xl mx-auto">
+      <div className="grid sm:grid-cols-1 lg:grid-cols-4  md:grid-cols-2 md:gap-6  gap-4 max-w-screen-xl mx-auto">
         {allInstructor?.data?.map((instructor) => {
           return (
             <div className="">
@@ -44,8 +39,7 @@ const Instructors = () => {
 
                       <div class="p-5">
                         <h5 class="text-green-600  dark:text-black font-bold text-md flex tracking-tight">
-                          <FaBookReader className="mt-1   text-black ml-1 mr-2" />{" "}
-                          {instructor?.background}
+                          <FaBookReader className="mt-1   text-black ml-1 mr-2" /> {instructor?.background}
                         </h5>
 
                         <p class="font-bold text-xl flex    text-gray-800">
@@ -55,8 +49,7 @@ const Instructors = () => {
                         </p>
 
                         <p class="font-bold text-md  mb-3 flex   text-gray-800">
-                          <BsHourglassSplit className="mt-1 text-xl mr-2" />{" "}
-                          {instructor?.experience} years experience
+                          <BsHourglassSplit className="mt-1 text-xl mr-2" /> {instructor?.experience} years experience
                         </p>
                         <div>
                           <Link to={`/instructor/${instructor?._id}`}>
@@ -64,9 +57,7 @@ const Instructors = () => {
                               <span class="circle" aria-hidden="true">
                                 <span class="icon arrow"></span>
                               </span>
-                              <span class="button-text dark:text-white">
-                                Learn More
-                              </span>
+                              <span class="button-text dark:text-white">Learn More</span>
                             </button>
                           </Link>
                         </div>
