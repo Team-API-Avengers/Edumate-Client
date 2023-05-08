15ada62f4ebfefcfe3d87a6aca08662c1af8e03a
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2023-05-08 15:11:17 +0600

    Skill test assessment route and question and answer setup with fuctionality completed

--- src/components/Pages/MyProfile/MyProfile.js
+++ src/components/Pages/MyProfile/MyProfile.js
@@ -1,13 +1,14 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 // import ProfileEditModal from "./ProfileEditModal";
 import { AuthContext } from "../../Context/AuthProvider";
+import { Link } from "react-router-dom";
 // import { BsFillBookmarkStarFill } from 'react-icons/bs';
 // import MyAccess from './MyProfileTable';
 
 const MyProfile = () => {
   const { user, logUser } = useContext(AuthContext);
 
-  const [editEmail, setEditEmail] = useState();
+  // const [editEmail, setEditEmail] = useState();
 
   console.log(logUser);
   return (
@@ -140,12 +141,14 @@ const MyProfile = () => {
                   </div>
                   <div class="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
                     <div class="py-6 px-3 mt-32 sm:mt-0">
+                      <Link to='/dashboard/skill-assessments'>
                       <button
                         class="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
                         type="button"
                       >
-                        Connect
+                        Skill Test
                       </button>
+                      </Link>
                     </div>
                   </div>
                   <div class="w-full lg:w-4/12 px-4 lg:order-1">
@@ -178,16 +181,16 @@ const MyProfile = () => {
                     </h3>
                   ) : (
                     <h3 class="text-4xl font-semibold leading-normal mb-2 text-blueGray-700 ">
-                      Name : <span className="font-bold">hhhhh</span>
+                      Name : <span className="font-bold">Guest</span>
                     </h3>
                   )}
                   {user?.email ? (
                     <h3 class="text-xl  font-semibold leading-normal mb-2 text-gray-700 ">
                       <span className="font-bold">{user?.email}</span>
-                    </h3>
+                    </h3> 
                   ) : (
                     <h3 class="text-4xl font-semibold leading-normal text-blueGray-700 mb-2">
-                      <span className="font-bold">hhhhh</span>
+                      <span className="font-bold">guest@gmail.com</span>
                     </h3>
                   )}
                   {logUser?.role && (
@@ -197,7 +200,7 @@ const MyProfile = () => {
                   <div class="mb-2 text-blue-600 ">
                     {logUser?._id && (
                       <p className=" text-small mb-2">
-                        User ID :{" "}
+                        User ID :
                         <span className=" font-bold my-10">
                           WBC-{logUser?._id?.slice(16, -1)}
                         </span>
