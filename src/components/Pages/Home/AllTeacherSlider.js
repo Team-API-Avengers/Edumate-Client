9fe84c2d382e6b74d32e03d9c64a55dc4d28d5a4
atik94 <atikurrahmanatik94@gmail.com>
2023-05-10 17:51:53 +0600

    teacher slider design complete

--- src/components/Pages/Home/AllTeacherSlider.js
+++ src/components/Pages/Home/AllTeacherSlider.js
@@ -1,136 +1,114 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
 const AllTeacherSlider = () => {
+  const [allTeachers, setAllTeachers] = useState([]);
 
-    const [allTeachers, setAllTeachers] = useState([])
+  useEffect(() => {
+    fetch(`https://edumate-second-server.vercel.app/api/v1/tutor`)
+      .then((res) => res.json())
+      .then((data) => setAllTeachers(data?.data));
+  }, []);
 
+  console.log(allTeachers);
 
-    useEffect(() => {
-        fetch(`https://edumate-second-server.vercel.app/api/v1/tutor`)
-        .then((res) => res.json())
-        .then((data) => setAllTeachers(data?.data))
-    },[])
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    duration: 2000,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+  };
 
-     console.log(allTeachers);
+  const mobile = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    duration: 2000,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+  };
+  return (
+    <div>
+      {/* Large */}
 
-        const settings = {
-          dots: true,
-          infinite: true,
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          autoplay: true,
-          speed: 2000,
-          duration: 2000,
-          autoplaySpeed: 2000,
-          cssEase: "linear"
-        };
-
-        const mobile = {
-          dots: true,
-          infinite: true,
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          autoplay: true,
-          speed: 2000,
-          duration: 2000,
-          autoplaySpeed: 2000,
-          cssEase: "linear"
-        };
-    return (
-        <div>
-            {/* Large */}
-            
-            <div className="hidden lg:block">
-            <div className='mx-5 lg:mx-10'>
-            <Slider {...settings}>
-             {
-                allTeachers?.map((teacher, idx) => 
-                <div key={idx}>
-                    <a href="#f" className="group relative block bg-black">
-                <img
+      <div className="hidden lg:block">
+        <div className="mx-5 lg:mx-10">
+          <Slider {...settings}>
+            {allTeachers?.map((teacher, idx) => (
+              <div key={idx}>
+                <a href="#f" className="group relative block bg-black">
+                  <img
                     alt="Developer"
                     src={teacher?.image}
                     className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
-                />
+                  />
 
-                <div className="relative p-4 sm:p-6 lg:p-8">
-                    <p className="text-sm font-medium uppercase tracking-widest text-pink-500">
-                    {teacher?.background}
-                    </p>
+                  <div className="relative p-4 sm:p-6 lg:p-8">
+                    <div>
+                      <p className="text-sm font-bold uppercase tracking-widest text-[#1AA3D0] dark:text[#00A99D]">
+                        {teacher?.background}
+                      </p>
 
-                    <p className="text-xl font-bold text-white sm:text-2xl">
-                    {teacher?.name}
-                    </p>
+                      <p className="text-xl font-bold text-white sm:text-2xl">{teacher?.name}</p>
+                    </div>
 
                     <div className="mt-32 sm:mt-48 lg:mt-64">
-                    <div
-                        className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100"
-                    >
-                        <p className="text-sm text-white">
-                        {teacher?.location}
-                        </p>
+                      <div className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100">
+                        <p className="text-sm text-white">{teacher?.location}</p>
+                      </div>
                     </div>
-                    </div>
-                </div>
+                  </div>
                 </a>
-                </div>
-                )
-             }
-        </Slider> 
+              </div>
+            ))}
+          </Slider>
         </div>
-            </div>
+      </div>
 
-
-
-
-
-
-            
-            {/* Mobile */}
-            <div className="lg:hidden block">
-            <div className='w-96 mx-auto'>
-            <Slider {...mobile}>
-             {
-                allTeachers?.map((teacher, idx) => 
-                <div key={idx}>
-                    <a href="#f" className="group relative block bg-black">
-                <img
+      {/* Mobile */}
+      <div className="lg:hidden block">
+        <div className="w-96 mx-auto">
+          <Slider {...mobile}>
+            {allTeachers?.map((teacher, idx) => (
+              <div key={idx}>
+                <a href="#f" className="group relative block bg-black">
+                  <img
                     alt="Developer"
                     src={teacher?.image}
                     className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
-                />
+                  />
 
-                <div className="relative p-4 sm:p-6 lg:p-8">
-                    <p className="text-sm font-medium uppercase tracking-widest text-pink-500">
-                    {teacher?.background}
+                  <div className="relative p-4 sm:p-6 lg:p-8">
+                    <p className="text-sm font-medium uppercase tracking-widest text-[#1AA3D0] dark:text[#00A99D]">
+                      {teacher?.background}
                     </p>
 
-                    <p className="text-xl font-bold text-white sm:text-2xl">
-                    {teacher?.name}
-                    </p>
+                    <p className="text-xl font-bold text-white sm:text-2xl">{teacher?.name}</p>
 
                     <div className="mt-32 sm:mt-48 lg:mt-64">
-                    <div
-                        className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100"
-                    >
-                        <p className="text-sm text-white">
-                        {teacher?.location}
-                        </p>
+                      <div className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100">
+                        <p className="text-sm text-white">{teacher?.location}</p>
+                      </div>
                     </div>
-                    </div>
-                </div>
+                  </div>
                 </a>
-                </div>
-                )
-             }
-        </Slider> 
+              </div>
+            ))}
+          </Slider>
         </div>
-            </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default AllTeacherSlider;
--- src/components/Pages/SkillAssessments/SkillAssessments.js
+++ src/components/Pages/SkillAssessments/SkillAssessments.js
@@ -1,35 +1,28 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from "react";
 // import { BsFillQuestionDiamondFill } from 'react-icons/bs';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import { AuthContext } from '../../Context/AuthProvider';
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { AuthContext } from "../../Context/AuthProvider";
 
 const SkillAssessments = () => {
+  const { user } = useContext(AuthContext);
 
-    const {user} = useContext(AuthContext)
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
-    
-    //! Change Style 
-    // const [formStyle, setFormStyle] = useState("block");
+  //! Change Style
+  // const [formStyle, setFormStyle] = useState("block");
 
+  const [questions, setQuestions] = useState([]);
+  const [data, setData] = useState([]);
 
+  useEffect(() => {
+    fetch(`https://edumate-second-server.vercel.app/api/v1/test`)
+      .then((res) => res.json())
+      .then((data) => setData(data?.data))
+      .finally(() => {});
+  }, []);
 
-    const [questions, setQuestions] = useState([])
-    const [data, setData] = useState([]);
-  
-
-    useEffect(() => {
-        fetch(`https://edumate-second-server.vercel.app/api/v1/test`)
-          .then((res) => res.json())
-          .then((data) => setData(data?.data))
-          .finally(() => {});
-      }, []);
-  
-        
-   
-  
-    // ! filtering data by category which is sorting
+  // ! filtering data by category which is sorting
   function handleFilter(event) {
     const query = event.target.value.toLowerCase();
     console.log(query);
@@ -38,39 +31,32 @@ const SkillAssessments = () => {
       return item?.category?.toLowerCase().includes(query);
     });
     // setFilteredData(filtered?.slice(0,4));
-  
+
     //! For Display 5 question from array by randomly .. .. ..
     const n = 10; // number of elements we want to get
     const shuffledArray = filtered.sort(() => 0.5 - Math.random()); // shuffles array
     const resultData = shuffledArray.slice(0, n + 2); // gets first n elements after shuffle
     setQuestions(resultData);
   }
-  
+
   console.log(questions);
-  
-  
-  
-  
-  
-  
-  
-  
-  // ! Check and count the number of correct answers for each question in the array 
-  
+
+  // ! Check and count the number of correct answers for each question in the array
+
   const [userAnswers, setUserAnswers] = useState(Array(questions.length).fill(null));
   // Set scores
   const [score, setScore] = useState();
-  
+
   function handleAnswerSelect(questionIndex, selectedOption) {
     const newAnswers = [...userAnswers];
     newAnswers[questionIndex] = selectedOption;
     setUserAnswers(newAnswers);
-  
+
     const updatedQuestions = [...questions];
     updatedQuestions[questionIndex].disabled = true;
     setQuestions(updatedQuestions);
   }
-  
+
   function calculateScore() {
     let newScore = 0;
     questions.forEach((question, index) => {
@@ -80,102 +66,78 @@ const SkillAssessments = () => {
     });
     setScore(newScore);
   }
-  
-
-
-
 
   // if(questions[1]){
   //   setFormStyle("hidden");
   // }
 
+  if (score === 8 || score > 8) {
+    toast.success(`Your score is ${score}`);
+    navigate("/dashboard");
+  }
+  if (score <= 7) {
+    toast.error(`Your score is ${score}`);
+    navigate("/");
+  }
 
-
-
-
-
-
-if(score === 3 || score > 3){
-  toast.success(`Your score is ${score}`)
-  navigate('/dashboard')
-}
-if(score === 0){
-  toast.error(`Your score is ${score}`)
-}
-  
-
-
-
-  
-  
-  
-
-    return (
-        <div className="mx-5 lg:mx-10">
-        {/* TODO: Selector */}
-        <div className="my">
+  return (
+    <div className="mx-5 lg:mx-10">
+      {/* TODO: Selector */}
+      <div className="my">
         <div>
-           <label className="block text-black dark:text-gray-300 text-xl font-semibold" id="title">
-             Hi <span>{user?.displayName}</span>
-             
-           </label>
-    
-           {/* Selector */}
-           <div>
-           <select
-             name="background"
-             // className="select  h-11 text-gray-800 border border-green-400 mt-1 rounded w-full bg-sky-50"
-             className="select  h-11 text-gray-800  mt-1 rounded w-full bg-sky-50 border-2  border-[#1AA3D0] dark:border-none focus:outline-none focus:border-[#00A99D]"
-             onChange={handleFilter}
-           >
-             <option disabled selected>
-               Choose your background
-             </option>
-             <option>Science</option>
-             <option>Commerce</option>
-             <option>Arts</option>
-           </select>
-           </div>
-         </div>
+          <label className="block text-black dark:text-gray-300 text-xl font-semibold" id="title">
+            Hi <span>{user?.displayName}</span>
+          </label>
+          <h2>{score}</h2>
+
+          {/* Selector */}
+          <div>
+            <select
+              name="background"
+              // className="select  h-11 text-gray-800 border border-green-400 mt-1 rounded w-full bg-sky-50"
+              className="select  h-11 text-gray-800  mt-1 rounded w-full bg-sky-50 border-2  border-[#1AA3D0] dark:border-none focus:outline-none focus:border-[#00A99D]"
+              onChange={handleFilter}
+            >
+              <option disabled selected>
+                Choose your background
+              </option>
+              <option>Science</option>
+              <option>Commerce</option>
+              <option>Arts</option>
+            </select>
+          </div>
         </div>
-    
-    
-    
-    
-        {/* TODO: Display the questions */}
-    {
-      questions[1] &&
-       <div>
-        {questions.map((question, index) => (
-         <div className="my-3" key={question.id}>
-           <p className="text-start bg-yellow-300">{question.question}</p>
-           {question.options.map((option, optionIndex) => (
-             <div className="text-start" key={optionIndex}>
-               <input
-                 type="radio"
-                 name={`question-${index}`}
-                 value={option}
-                 checked={userAnswers[index] === option}
-                 disabled={question.disabled && userAnswers[index] !== option}
-                 onChange={() => handleAnswerSelect(index, option)}
-               />
-               <label>{option}</label>
-             </div>
-           ))}
-         </div>
-       ))}
-    
-    
-    
-       <button className="btn btn-primary my-10" onClick={calculateScore}>Submit</button>
-       </div>
-    }
-    
-    
-    
-       
-     </div>
-    );
+      </div>
+
+      {/* TODO: Display the questions */}
+      {questions[1] && (
+        <div>
+          {questions.map((question, index) => (
+            <div className="my-3" key={question.id}>
+              <p className="text-start bg-yellow-300">{question.question}</p>
+              {question.options.map((option, optionIndex) => (
+                <div className="text-start" key={optionIndex}>
+                  <input
+                    type="radio"
+                    name={`question-${index}`}
+                    value={option}
+                    checked={userAnswers[index] === option}
+                    disabled={question.disabled && userAnswers[index] !== option}
+                    onChange={() => handleAnswerSelect(index, option)}
+                  />
+                  <label>{option}</label>
+                </div>
+              ))}
+            </div>
+          ))}
+
+          <button className="btn btn-primary my-10" onClick={calculateScore}>
+            Submit
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default SkillAssessments;
